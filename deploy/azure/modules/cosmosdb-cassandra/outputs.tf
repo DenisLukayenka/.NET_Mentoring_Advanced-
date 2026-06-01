3c4f7aeaa3eab4700f1509468383fb45cd49e7fe
dzianis.lukayenka <dav08649@gmail.com>
2026-06-01 18:59:51 +0200

    fix: fit db outputs

--- deploy/azure/modules/cosmosdb-cassandra/outputs.tf
+++ deploy/azure/modules/cosmosdb-cassandra/outputs.tf
@@ -10,7 +10,7 @@ output "primary_key" {
 }
 
 output "primary_connection_string" {
-  value       = var.resource_count > 0 ? azurerm_cosmosdb_account.job_outputs[0].connection_strings[0] : ""
+  value       = var.resource_count > 0 ? "AccountEndpoint=${azurerm_cosmosdb_account.job_outputs[0].endpoint};AccountKey=${azurerm_cosmosdb_account.job_outputs[0].primary_key};" : ""
   sensitive   = true
   description = "Cosmos DB Cassandra account primary connection string."
 }
