f278eaa6bf5f4a329167e31fc085c0f99f0bd271
dzianis.lukayenka <dav08649@gmail.com>
2026-06-02 15:06:39 +0200

    fix: attempt 3

--- deploy/azure/modules/documentdb/outputs.tf
+++ deploy/azure/modules/documentdb/outputs.tf
@@ -1,9 +1,9 @@
 output "primary_connection_string" {
-  value     = var.resource_count > 0 ? azurerm_mongo_cluster.primary[0].connectionString : ""
+  value     = var.resource_count > 0 ? azurerm_mongo_cluster.primary[0].connectionStrings[0].connection_string : ""
   sensitive = true
 }
 
 output "replica_connection_string" {
-  value     = var.resource_count > 0 ? azurerm_mongo_cluster.replica[0].connectionString : ""
+  value     = var.resource_count > 0 ? azurerm_mongo_cluster.replica[0].connectionStrings[0].connection_string : ""
   sensitive = true
 }
