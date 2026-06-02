454c97105e4b71b87291281a28310878935dc7b6
dzianis.lukayenka <dav08649@gmail.com>
2026-06-02 15:01:19 +0200

    fix: update terraform config

--- deploy/azure/modules/documentdb/outputs.tf
+++ deploy/azure/modules/documentdb/outputs.tf
@@ -1,9 +1,9 @@
 output "primary_connection_string" {
-  value     = var.resource_count > 0 ? azurerm_mongo_cluster.primary[0].connection_strings[0] : ""
+  value     = var.resource_count > 0 ? azurerm_mongo_cluster.primary.connection_strings[0].value : ""
   sensitive = true
 }
 
 output "replica_connection_string" {
-  value     = var.resource_count > 0 ? azurerm_mongo_cluster.replica[0].connection_strings[0] : ""
+  value     = var.resource_count > 0 ? azurerm_mongo_cluster.replica.connection_strings[0].value : ""
   sensitive = true
 }
